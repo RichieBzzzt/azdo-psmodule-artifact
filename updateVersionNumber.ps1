5ac12d29fe876b1c83091e53ac910b41e6706aec
Richie Lee <richie@bzzzt.io>
2020-04-06 13:09:46 +0100

    paths!

--- updateVersionNumber.ps1
+++ updateVersionNumber.ps1
@@ -1,10 +1,10 @@
 param(
     $versionNumber 
 )
-$manifest = Import-PowerShellDataFile .\noddyModule.psd1 
+$manifest = Import-PowerShellDataFile .\noddyModule\noddyModule.psd1 
 [version]$version = $Manifest.ModuleVersion
 if ([string]::IsNullorEmpty( $versionNumber)) {
     $versionNumber = $Version.Build + 1
 }
 [version]$NewVersion = "{0}.{1}.{2}" -f $Version.Major, $Version.Minor, $versionNumber
-Update-ModuleManifest -Path .\noddyModule.psd1 -ModuleVersion $NewVersion
+Update-ModuleManifest -Path .\noddyModule\noddyModule.psd1 -ModuleVersion $NewVersion
